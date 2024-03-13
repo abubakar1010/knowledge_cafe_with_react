52f431dc2f541c0e5cc82c55b3faffb267372c80
Abubakar1010 <abubakar850772@gmail.com>
2024-03-13 20:53:58 +0600

    display bookmark

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmark from './components/Bookmark/Bookmark'
 import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
+    <div className=' container mx-auto'>
+
     <Header />
+    <div className=' flex gap-7'>
     <Blogs />
+    <Bookmark />
+
+    </div>
+    </div>
     </>
   )
 }
